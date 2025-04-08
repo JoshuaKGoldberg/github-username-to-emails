a78eb3532faa2f485fa28d7bc878f7d6f69333fd
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2025-04-08 09:57:23 -0500

    chore: bump create-typescript-app to 2.35.1 and remove its config (#577)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #574
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/github-username-to-emails/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/github-username-to-emails/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    📧

--- eslint.config.js
+++ eslint.config.js
@@ -32,7 +32,7 @@ export default tseslint.config(
 			tseslint.configs.strictTypeChecked,
 			tseslint.configs.stylisticTypeChecked,
 		],
-		files: ["**/*.js", "**/*.ts"],
+		files: ["**/*.{js,ts}"],
 		languageOptions: {
 			parserOptions: {
 				projectService: {
